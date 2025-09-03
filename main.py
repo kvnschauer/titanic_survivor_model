5ea72129c20657edb7c338a9266f4125174ea066
Kevin <kvnschauer@protonmail.com>
2025-09-02 23:14:25 -0600

    fine tune random forest, predict results

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ trn_data = pd.read_csv('./Data/train.csv')
 analyzer = Analyzer(trn_data)
 model = Model(trn_data)
 
+continue_input = True
 user_input = None
-valid_user_input = {'1': analyzer.analyze_data, '2': model.train_model}
+valid_user_input = {'1': analyzer.analyze_data, '2': lambda: model.train_model(save_model=True), '3': model.predict}
 
-while user_input not in valid_user_input:
-    user_input = input('Enter: \n'
-                    '1. To analyze the data\n'
-                    '2. To train the model\n')
+while continue_input:
+    while user_input not in valid_user_input:
+        user_input = input('Enter: \n'
+                        '1. To analyze the data\n'
+                        '2. To train the model\n'
+                        '3. To predict with the model\n')
 
-valid_user_input[user_input]()
+    valid_user_input[user_input]()
+
+    while user_input != 'y' and user_input != 'n':
+        user_input = input('Do you want to continue? (y/n)\n').lower()
+
+    if user_input == 'n':
+        continue_input = False
--- model.py
+++ model.py
@@ -1,18 +1,20 @@
+from datetime import datetime
 
+import joblib
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from pandas import DataFrame
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import SGDClassifier, LinearRegression
-from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, MinMaxScaler, minmax_scale
-from sklearn.svm import LinearSVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, MinMaxScaler
 
 class Model():
     data: DataFrame
+    model_file_name = 'rand_forest_titanic_classifier.pkl'
+    preprocessor: ColumnTransformer
 
     def __init__(self, data):
         self.data = data.copy()
@@ -64,22 +66,35 @@ class Model():
             data['Fare'] = np.log(data['Fare'])
             return  data
 
-        return ColumnTransformer(transformers=[
-            ('Hot_encoded', OneHotEncoder(sparse_output=True), ['Sex', 'Parch', 'SibSp', 'Pclass', 'Embarked', 'TicketPrefix']),
-            ('Fare', Pipeline([
-                ('log_transform', FunctionTransformer(func=fare_log_transform)),
-                ('min_max', MinMaxScaler())
-            ]), ['Fare']),
-            ('MinMax', MinMaxScaler(), ['TicketNumber', 'Age']),
-            ('drop_cols', 'drop', ['Name', 'Ticket', 'Cabin', 'Survived'])
-        ])
+        # feature importance is very low for any parch value other than 0 or 1, combine all others into a value '2'
+        def __reduce_parch_noise(data: DataFrame):
+            data['Parch'] = np.where(data['Parch'] > 2, 2, data['Parch'])
+            return data
 
-    def train_model(self):
+        def __reduce_sibsp_noise(data: DataFrame):
+            data['SibSp'] = np.where(data['SibSp'] > 2, 2, data['SibSp'])
+            return data
+
+        return ColumnTransformer(transformers=[
+            ('Hot_encoded', OneHotEncoder(sparse_output=True, handle_unknown='ignore'), ['Sex', 'Pclass', 'Embarked']),
+            ('Parch', Pipeline([
+                 ('reduce_noise', FunctionTransformer(func=__reduce_parch_noise, feature_names_out='one-to-one')),
+                 ('hot_encode', OneHotEncoder(sparse_output=True, handle_unknown='ignore'))
+            ]), ['Parch']),
+            ('SibSp', Pipeline([
+                ('reduce_noise', FunctionTransformer(func=__reduce_sibsp_noise, feature_names_out='one-to-one')),
+                ('hot_encode', OneHotEncoder(sparse_output=True, handle_unknown='ignore'))
+            ]), ['SibSp']),
+            ('MinMax', MinMaxScaler(), ['TicketNumber', 'Age']),
+            ('passthrough', 'passthrough', ['Fare']),# since we are using random forest, no need to scale
+            ('drop_cols', 'drop', ['Name', 'Ticket', 'Cabin'])
+        ], remainder='drop')
+
+    def train_model(self, save_model = False):
         """
             Handles training the model and analyzing performance
             :return: None
         """
-
         self.__ticket_prefix_feature_extraction(self.data)
         self.__ticket_number_feature_extraction(self.data)
 
@@ -95,19 +110,43 @@ class Model():
         self.data.Age = np.where((self.data.Sex == 'male') & (pd.isnull(self.data.Age)), male_average_age, self.data['Age'])
 
         pre_processor = self.__get_preprocessor()
-        X = pre_processor.fit_transform(self.data)
+        self.preprocessor = pre_processor.fit(self.data)
+        X = pre_processor.transform(self.data)
         y = self.data.loc[:, 'Survived']
 
-        #SGDClassifier
-        #LinearRegression
-        #LinearSVC
-        #DecisionTreeClassifier
+        print(f'feature names: {pre_processor.get_feature_names_out()}')
 
-        sgd_classifier = SGDClassifier()
-        linear_regressor = LinearRegression()
-        svc = LinearSVC()
-        decision_tree = DecisionTreeClassifier()
+        rand_forest = RandomForestClassifier(random_state=22, n_jobs=-1, oob_score=True, max_depth=10, min_samples_split=6, n_estimators=100, max_features="log2")
 
-        scores = cross_val_score(decision_tree, X, y, cv=5)
+        rand_forest.fit(X, y)
 
-        print(scores)
+        print(f'Train score: {accuracy_score(rand_forest.predict(X), y)}')
+        print(f'OOB score: {rand_forest.oob_score_}')
+
+        if save_model:
+            joblib.dump(rand_forest, self.model_file_name)
+
+    def predict(self):
+        # for now we'll assume it's a random forest classifier since that is what we've been working with
+        predictor: RandomForestClassifier = joblib.load(self.model_file_name)
+
+        test_data = pd.read_csv('./Data/test.csv')
+
+        print(test_data.shape)
+
+        self.__ticket_number_feature_extraction(test_data)
+
+        X = self.preprocessor.transform(test_data)
+
+        predictions = predictor.predict(X)
+
+        prediction_df = pd.DataFrame({'PassengerId': test_data['PassengerId'], 'Survived': predictions})
+
+        date_time_now = datetime.now()
+        day_time = '{}_{}_{}_{}_{}_{}'.format(date_time_now.month,
+                                                 date_time_now.day,
+                                                 date_time_now.year,
+                                                 date_time_now.hour,
+                                                 date_time_now.minute,
+                                                 date_time_now.second)
+        prediction_df.to_csv(f'./Data/predictions_{day_time}.csv', index=False)
